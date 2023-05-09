e1e90cbb6621e55e682cd1d4974891f52bd74929
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-09 13:29:03 +0000

    fixed error in 2 and 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for ascii_value in range(97, 123):
-    print(chr(ascii_value), end='')
+    print(f'{chr(ascii_value)}', end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_value in range(97, 123):
     if chr(ascii_value) != 'e' and chr(ascii_value) != 'q':
-        print(chr(ascii_value), end='')
+        print(f'{chr(ascii_value)}', end='')
