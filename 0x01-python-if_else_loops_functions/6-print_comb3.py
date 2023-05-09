d98096627fce658ac1314002c2da268dfe6cb3b6
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-09 13:40:48 +0000

    fixed error in 6 and 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num_1 in range(0, 10):
     for num_2 in range(num_1 + 1, 10):
         if num_1 == 8 and num_2 == 9:
-            print(f"{num_1}{num_2}")
+            print("{}{}".format(num_1, num_2))
         else:
-            print(f"{num_1}{num_2}", end=", ")
+            print("{}{}".format(num_1, num_2), end=", ")
