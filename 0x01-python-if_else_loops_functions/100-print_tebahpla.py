03f17a7a4a5e5c48b9cc0d2740c1454ffb4ef4b2
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-09 15:48:19 +0000

    fixed error in file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 i = 122
 
 while (i >= 65 and i < 91) or (i > 96 and i < 123):
-    print(chr(i), end='')
+    print('{}'.format(chr(i)), end='')
     if i > 96 and i < 123:
         i = i - 33
     elif i > 64 and i < 91:
