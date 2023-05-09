56b4a4857aeda318252e388e1559cdf1b5daa3d8
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-09 13:37:38 +0000

    fixed error in 4 and 5

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for counter in range(0, 99):
-    print(f'{counter:d} = {hex(counter)}')
+    print('{} = {}'.format(counter, hex(counter)))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
     if number == 99:
-        print(f'{number}')
+        print('{}'.format(number))
     else:
         print(f'{number:02}', end=', ')
