a4029e64090ef316663c90b40ef90f0b260d7745
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-15 04:25:18 -0700

    fixed error in 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
 
     new_list = [i for i in my_list]
