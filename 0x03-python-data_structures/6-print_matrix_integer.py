1fef9897af32ac1d7788a443d5e6ca6214059066
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-15 04:33:24 -0700

    fixed error in 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
+            print('{:d}'.format(j), end='')
             if i[len(i) - 1] != j:
-                print('{:d}'.format(j), end=' ')
-            else:
-                print('{:d}'.format(j))
+                print(' ', end='')
+        print('')
