a80b523e095c7a4d3d1b5d1cdd45706689f3ae2a
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-17 17:28:34 +0000

    fixed error by removing extra line in file 1 and 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,11 @@ import sys
 
 if __name__ == '__main__':
     args_length = len(sys.argv) - 1
-    
     if args_length == 1:
         print('{} argument:'.format(args_length))
     elif args_length == 0:
         print('{} arguments.'.format(args_length))
     elif args_length > 1:
         print('{} arguments:'.format(args_length))
-    
     for counter in range(args_length):
         print('{}: {}'.format(counter + 1, sys.argv[counter + 1]))
