b7ec984c67fbadf51eed4230248cdd6a8c2666a5
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-05-29 14:07:36 +0000

    Added advanced task file 102 and fixed all minor errors(pycodestyle) in files

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print('out of range')
         finally:
-            returning_list.append(result)            
+            returning_list.append(result)
     return returning_list
