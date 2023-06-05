24bacc191c20f1e1f39e8fde2d461192ec2b37f1
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2023-06-05 17:21:40 +0100

    fixed error in 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -93,12 +93,3 @@ class Rectangle:
         '''returns a string if an instance is deleted'''
         Rectangle.number_of_instances -= 1
         print('Bye rectangle...')
-
-
-my_rectangle_1 = Rectangle(2, 4)
-my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_1
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_2
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
