d0715650336abf39ce108e5c3192daf668a37ff5
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2023-06-05 14:45:14 +0100

    fixed pycodestyle error in 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
