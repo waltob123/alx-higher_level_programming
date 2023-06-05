35f2d4da4dd6bb33f96fb3fc8a0cb489caacbff7
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2023-06-05 14:23:31 +0100

    Fixed error in 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,8 +30,8 @@ class Rectangle:
         # check if width is greater than 0
         if height < 0:
             raise ValueError('height must be >= 0')
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
