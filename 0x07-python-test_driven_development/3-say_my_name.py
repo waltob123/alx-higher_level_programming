91d57478924e32811de6fe85057d41d832d27110
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-06-07 13:00:39 +0000

    fixed error in 3-say*.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,7 @@
 
 def say_my_name(first_name, last_name=""):
     '''
-    A function which returns your firstname and lastname
+    A function which prints your firstname and lastname
     together.
 
     Args:
@@ -21,4 +21,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
 
-    return 'My name is {} {}'.format(first_name, last_name)
+    print('My name is {} {}'.format(first_name, last_name))
