bd53f4ca4c643579a3ce2549de8d6626c6c356ba
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-06-07 13:32:57 +0000

    fixed error in 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -22,8 +22,6 @@ def print_square(size):
     if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
 
-    if size == 0:
-        print('')
-    else:
-        for i in range(size):
-            print(size * '#')
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
