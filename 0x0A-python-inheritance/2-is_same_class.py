9ef63e58345ea06a88e346d6e7ec5d09c3bcc941
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-06-12 13:48:00 +0000

    fixed errors in 2 and 5

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -15,6 +15,6 @@ def is_same_class(obj, a_class):
     Return:
         True or False
     '''
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -3,4 +3,5 @@
 
 
 class BaseGeometry:
+    '''Defines a BaseGeometry'''
     pass
