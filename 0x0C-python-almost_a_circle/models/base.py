0eb3e95daa421dcda22b16e1e561842b6497730c
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-06-19 11:07:03 +0000

    Finsihed the mandatory tasks for python almost a circle
    Added the solution for tasks 18 and 19.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,13 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        '''
+        converts a list of dictionaries to a json string
+        Args:
+            list_dictionaries(list): list of dictionaries to convert
+        Return:
+            json string
+        '''
         if not isinstance(list_dictionaries, list) or \
                 len(list_dictionaries) == 0 or list_dictionaries is None:
             return '[]'
@@ -25,7 +32,12 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        filename = cls.__class__.__name__ + '.json'
+        '''
+        saves a list of objects to a json file
+        Args:
+            list_objs(list): list of objects to save
+        '''
+        filename = str(cls.__name__) + '.json'
         with open(filename, 'w') as f:
             if list_objs is None:
                 f.write('[]')
@@ -35,6 +47,38 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        '''
+        converts a json string to a python object
+        Args:
+            json_string(str): json string to convert
+        Return:
+            python object
+        '''
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''
+        creates a new instance of the class using the key-worded
+        argument dictionary
+        '''
+        if dictionary and len(dictionary) > 0:
+            if cls.__name__ == 'Rectangle':
+                new_instance = cls(5, 4)
+            elif cls.__name__ == 'Square':
+                new_instance = cls(5)
+            new_instance.update(**dictionary)
+            return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        '''loads a list of dictionaries from a json file'''
+        filename = str(cls.__name__) + '.json'
+        try:
+            with open(filename, 'r') as f:
+                list_of_dictionaries = Base.to_json_string(f.read())
+                return [cls.create(**item) for item in list_of_dictionaries]
+        except (FileNotFoundError):
+            return []
