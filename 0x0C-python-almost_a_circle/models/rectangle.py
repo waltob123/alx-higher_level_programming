6eec0855b2ddb18b3356dd4f16454042a1124cbe
Kwasi Asamoah <walterobrien_123@yahoo.com>
2023-06-19 19:53:09 +0000

    fixed errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -165,5 +165,5 @@ class Rectangle(Base):
         }
 
     def __str__(self):
-        return (f'[Rectangle] ({self.id}) {self.x}/{self.y}\
+        return (f'[Rectangle] ({self.id}) {self.x}/{self.y} - \
 {self.width}/{self.height}')
