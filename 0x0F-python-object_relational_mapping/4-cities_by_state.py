98445343af1512f5a0d5c049129a26b2f7988105
Desmond Asiedu Asamoah <walterobrien_123@yahoo.com>
2023-08-21 01:03:34 +0100

    Fixed error in file 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.excute("SELECT `c`.`id`, `c`.`name`, `s`.`name` FROM`
-            cities`  as c INNER JOIN `states` as `s` ON `c`.`id`
+    cursor.excute("SELECT `c`.`id`, `c`.`name`, `s`.`name` FROM`\
+            cities`  as c INNER JOIN `states` as `s` ON `c`.`id`\
             = `s`.`id` ORDER BY `cities`.`id` ASC")
     [print(city) for city in cursor.fetchall()]
